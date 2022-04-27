6650ac9c56646fb3d4df92263fdf845accac78dc
John Zhou <realjohnzhou@outlook.com>
2022-04-27 20:40:20 +0800

    feat: optimization

---
+++ john-demo/src/main/java/com/john/boot/demo/JohnDemoApplication.java
@@ -0,0 +1,13 @@
+package com.john.boot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JohnDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JohnDemoApplication.class, args);
+    }
+
+}
---
+++ john-demo/src/test/java/com/john/boot/demo/JohnDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.john.boot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JohnDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
