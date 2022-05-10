1fc3f68257587d934707fc679201a0b6a84bf82b
John Zhou <realjohnzhou@outlook.com>
2022-05-10 18:39:51 +0800

    feat: remove swagger annotation

--- john-boot-starter-web/src/main/java/com/john/boot/web/WebConfiguration.java
+++ john-boot-starter-web/src/main/java/com/john/boot/web/WebConfiguration.java
@@ -1,12 +1,10 @@
 package com.john.boot.web;
 
 import org.springframework.context.annotation.ComponentScan;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author john
  */
-@EnableSwagger2
 @ComponentScan
 public class WebConfiguration {
 }
