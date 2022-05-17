4cd86354419ec1a20751b434b387c8097bded2d1
John Zhou <realjohnzhou@outlook.com>
2022-05-17 18:35:37 +0800

    feat: optimize Result

--- john-boot-starter-common/src/main/java/com/john/boot/common/dto/Result.java
+++ john-boot-starter-common/src/main/java/com/john/boot/common/dto/Result.java
@@ -6,47 +6,47 @@ import lombok.Data;
  * @author john
  */
 @Data
-public class Result {
+public class Result<T> {
     public static final Integer OK = 1;
 
     public static final Integer NO = 0;
 
     private Integer code;
 
-    private Object data;
+    private T data;
 
     private String message;
 
     private Result() {
     }
 
-    public static Result success(Integer code, Object data) {
-        Result result = new Result();
+    public static <T> Result<T> success(Integer code, T data) {
+        Result<T> result = new Result<>();
         result.setCode(code);
         result.setData(data);
         return result;
     }
 
-    public static Result success(Object data) {
+    public static <T> Result<T> success(T data) {
         return Result.success(Result.OK, data);
     }
 
-    public static Result success() {
+    public static <T> Result<T> success() {
         return Result.success(Result.OK, null);
     }
 
-    public static Result failure(Integer code, String message) {
-        Result result = new Result();
+    public static <T> Result<T> failure(Integer code, String message) {
+        Result<T> result = new Result<>();
         result.setCode(code == null ? Result.NO : code);
         result.setMessage(message);
         return result;
     }
 
-    public static Result failure(String message) {
+    public static <T> Result<T> failure(String message) {
         return Result.failure(Result.NO, message);
     }
 
-    public static Result failure() {
+    public static <T> Result<T> failure() {
         return Result.failure(Result.NO, null);
     }
 }
