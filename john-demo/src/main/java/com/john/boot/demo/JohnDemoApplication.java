2bd07b58b98cc43c2cc4b4c9f42e6f1973f4e60a
John Zhou <realjohnzhou@outlook.com>
2022-06-12 16:48:12 +0800

    doc: add doc

--- john-demo/src/main/java/com/john/boot/demo/JohnDemoApplication.java
+++ john-demo/src/main/java/com/john/boot/demo/JohnDemoApplication.java
@@ -3,6 +3,9 @@ package com.john.boot.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author John Zhou
+ */
 @SpringBootApplication
 public class JohnDemoApplication {
 
