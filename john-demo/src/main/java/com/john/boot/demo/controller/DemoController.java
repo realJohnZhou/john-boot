45c520f608a209d8b9e6523e39b144a20c8bb6bb
John Zhou <realjohnzhou@outlook.com>
2022-07-14 22:40:25 +0800

    doc: add doc

--- john-demo/src/main/java/com/john/boot/demo/controller/DemoController.java
+++ john-demo/src/main/java/com/john/boot/demo/controller/DemoController.java
@@ -2,6 +2,7 @@ package com.john.boot.demo.controller;
 
 import com.john.boot.common.dto.AuthUser;
 import com.john.boot.common.util.AuditUserContextHolder;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/demo")
 public class DemoController {
 
+    @ApiOperation("get me")
     @GetMapping
     public AuthUser getMe() {
         return AuditUserContextHolder.getUser();
